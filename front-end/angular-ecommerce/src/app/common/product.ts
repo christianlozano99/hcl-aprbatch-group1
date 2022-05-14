9a870eb6e4591a4a4c246e42ab0427bfbb0ae5e8
MrVinegar <vincex568@icloud.com>
2022-05-14 11:19:26 -0400

    working code

---
+++ front-end/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,15 @@
+export class Product {
+  sku: string;
+  name: string;
+  artistName: string;
+  description: string;
+  tracks: number;
+  yearPublished: string;
+  unitPrice: number;
+  imageUrl: string;
+  active: boolean;
+  unitsInStock: number;
+  dateCreated: Date;
+  lastUpdate: Date;
+  id: string;
+}
